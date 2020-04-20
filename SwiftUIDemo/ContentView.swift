02b5162f994d55f686b8e65d76fb50ef353fb070
FD <lihxCode@gmail.com>
2020-04-20 23:14:47 +0800

    初始化

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by FD on 2020/4/20.
+//  Copyright © 2020 FD. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
